384e9eeee23e460e6317e432557ffae8a3434d5c
Duanchen Liu <liuaha@bc.edu>
2019-11-07 13:03:41 -0500

    update

--- js/main.js
+++ js/main.js
@@ -6,7 +6,16 @@ document.querySelector(".select-control").addEventListener("change", updateBarCh
 function updateBarChart(){
   let column = document.querySelector(".select-control").value;
   let spec = {
-   
+    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+    "title": "Coffee House Chains",
+    "width":450,
+    "height":300,
+    "data": {"url":"data/coffee-house-chains.csv"},
+    "mark": "bar",
+    "encoding": {
+      "x": {"field": "company", "type": "nominal", "sort": "-y"},
+      "y": {"field": column, "type": "quantitative", "sort": "-x"}
+    }
   };
   vegaEmbed('#chart-area', spec);
 }
